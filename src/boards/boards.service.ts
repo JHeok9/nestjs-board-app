0d600bae2a0825a45a247815bda8907175c76207
JHeok <skdhd999@gmail.com>
2024-07-15 17:26:21 +0900

    getBoardById 결과없을시 예외처리

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Board, BoardStatus } from './board.model';
 import { v1 as uuid } from 'uuid';
 import { CreateBoardDto } from './dto/create-board.dto';
@@ -31,7 +31,13 @@ export class BoardsService {
 
     // 특정게시글 불러오기
     getBoardById(id: string): Board {
-        return this.boards.find((board) => board.id === id)
+        const found = this.boards.find((board) => board.id === id);
+
+        if(!found) {
+            throw new NotFoundException(`Can't find Board with id ${id}`);
+        }
+
+        return found;
     }
 
     // 특정게시글 지우기
