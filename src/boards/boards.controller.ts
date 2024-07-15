2e5df0d0a2480c7b4466e09872f17f1fa2e8744d
JHeok <skdhd999@gmail.com>
2024-07-15 12:21:50 +0900

    BoardService 생성

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,6 +1,13 @@
 import { Controller } from '@nestjs/common';
+import { BoardsService } from './boards.service';
 
 @Controller('boards')
 export class BoardsController {
-    
+    // BoardService 사용하기위한 Dependency Injection
+    constructor(private boardService: BoardsService) {}
+
+    // Read 모든게시물 데이터 가져오기
+    getAllTask(){
+        this.boardService
+    }
 }
