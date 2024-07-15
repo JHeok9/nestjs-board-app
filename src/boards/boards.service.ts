cd83bc39e5dd3d2d8b3d63f5587335ec3439bf24
JHeok <skdhd999@gmail.com>
2024-07-15 12:45:33 +0900

    게시글 생성 서비스

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,12 +1,27 @@
 import { Injectable } from '@nestjs/common';
-import { Board } from './board.model';
+import { Board, BoardStatus } from './board.model';
+import { v1 as uuid } from 'uuid';
 
 @Injectable()
 export class BoardsService {
     private boards: Board[] = [];
 
+    // 전체게시글 불러오기
     getAllBoards(): Board[] {
         return this.boards;
+    }
+
+    // 게시글 생성하기
+    createBoard(title: string, description: string) {
+        const board: Board = {
+            id: uuid(),
+            title,
+            description,
+            status: BoardStatus.PUBLIC
+        }
+
+        this.boards.push(board);
+        return board;
     }
 
 }
