fd63dfceafcc21f1ae4aabc0975aefdb55659309
JHeok <skdhd999@gmail.com>
2024-07-16 18:25:46 +0900

    JWT

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Post, Req, UseGuards, ValidationPipe } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthCredentialsDto } from './dto/auth-credential.dto';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
 export class AuthController {
@@ -18,6 +19,13 @@ export class AuthController {
     @Post('/signin')
     signIn(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto): Promise <{accessToken: string}> {
         return this.authService.signIn(authCredentialsDto)
+    }
+
+    // JWT토큰 테스트
+    @Post('/test')
+    @UseGuards(AuthGuard())
+    test(@Req() req) {
+        console.log('req', req);
     }
 
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,6 +5,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserReopository } from './user.repository';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
     imports: [
@@ -18,6 +19,7 @@ import { PassportModule } from '@nestjs/passport';
         TypeOrmModule.forFeature([UserReopository])
     ],
     controllers: [AuthController],
-    providers: [AuthService, UserReopository]
+    providers: [AuthService, UserReopository, JwtStrategy],
+    exports:[JwtStrategy, PassportModule]
 })
 export class AuthModule {}
