260df2c66511e494143622409819616921c4e887
JHeok <skdhd999@gmail.com>
2024-07-16 17:47:57 +0900

    로그인시 JWT토큰 반환

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,12 +3,14 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { UserReopository } from './user.repository';
 import { AuthCredentialsDto } from './dto/auth-credential.dto';
 import * as bcrypt from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
     constructor(
         @InjectRepository(UserReopository)
         private userRepository: UserReopository,
+        private jwtService: JwtService
     ) {}
 
     // 유저 회원가입
@@ -17,12 +19,16 @@ export class AuthService {
     }
 
     // 유저 로그인
-    async signIn(authCredentialsDto: AuthCredentialsDto): Promise <String> {
+    async signIn(authCredentialsDto: AuthCredentialsDto): Promise <{accessToken: string}> {
         const {username, password} = authCredentialsDto;
         const user = await this.userRepository.findOneBy({ username });
 
         if(user && (await bcrypt.compare(password, user.password))){
-            return 'logIn success'
+            // 유저 토큰 생성( Secret + Payload )
+            const payload = { username };
+            const accessToken = await this.jwtService.sign(payload);
+
+            return {accessToken};
         } else {
             throw new UnauthorizedException('logIn failed')
         }
