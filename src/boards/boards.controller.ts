1884c1c888eb654d8839dcdde972fd0e097720e4
JHeok <skdhd999@gmail.com>
2024-07-16 12:03:59 +0900

    게시글 생성, 불러오기

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -3,46 +3,45 @@ import { BoardsService } from './boards.service';
 import { BoardStatus } from './board-status.enum';
 import { CreateBoardDto } from './dto/create-board.dto';
 import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe';
+import { Board } from './board.entity';
 
 @Controller('boards')
 export class BoardsController {
     // BoardService 사용하기위한 Dependency Injection
     constructor(private boardService: BoardsService) {}
 
-    // Read 모든게시물 데이터 가져오기
-    @Get('/')
-    getAllBoard(): Board[] {
-        return this.boardService.getAllBoards();
-    }
+    // // Read 모든게시물 데이터 가져오기
+    // @Get('/')
+    // getAllBoard(): Board[] {
+    //     return this.boardService.getAllBoards();
+    // }
 
     // Create 게시글 생성하기
     @Post()
     @UsePipes(ValidationPipe)
-    createBoard(
-        @Body() createBoardDto: CreateBoardDto
-    ): Board {
-        return this.boardService.createBoard(createBoardDto);
+    createBoard(@Body() creatBoardDto: CreateBoardDto): Promise <Board>{
+        return this.boardService.createBoard(creatBoardDto);
     }
 
     // Read 특정 게시글 데이터 가져오기
     @Get('/:id') // localhost/boards/{id값}
-    getBoardById(@Param('id') id: string): Board{
+    getBoardById(@Param('id') id: number): Promise <Board>{
         return this.boardService.getBoardById(id);
     }
 
-    // Delete 특정 게시글 데이터 지우기
-    @Delete('/:id')
-    deleteBoard(@Param('id') id: string): void{
-        this.boardService.deleteBoard(id);
-    }
+    // // Delete 특정 게시글 데이터 지우기
+    // @Delete('/:id')
+    // deleteBoard(@Param('id') id: string): void{
+    //     this.boardService.deleteBoard(id);
+    // }
 
-    // Update 특정 게시글 업데이트
-    @Patch('/:id/status')
-    updateBoardStatus(
-        @Param('id') id: string,
-        @Body('status', BoardStatusValidationPipe) status: BoardStatus
-    ): Board {
-        return this.boardService.updateBoardStatus(id, status);
-    }
+    // // Update 특정 게시글 업데이트
+    // @Patch('/:id/status')
+    // updateBoardStatus(
+    //     @Param('id') id: string,
+    //     @Body('status', BoardStatusValidationPipe) status: BoardStatus
+    // ): Board {
+    //     return this.boardService.updateBoardStatus(id, status);
+    // }
 
 }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -4,59 +4,49 @@ import { v1 as uuid } from 'uuid';
 import { CreateBoardDto } from './dto/create-board.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { BoardRepository } from './board.repository';
+import { Board } from './board.entity';
 
 @Injectable()
 export class BoardsService {
-    // private boards: Board[] = [];
+    // BoardRepository 주입
     constructor(
         @InjectRepository(BoardRepository)
         private boardRepository: BoardRepository,
     ) {}
 
     // 전체게시글 불러오기
-    getAllBoards(): Board[] {
-        return this.boards;
-    }
+    
 
     // 게시글 생성하기
-    createBoard(createBoardDto: CreateBoardDto) {
-        // const title = createBoardDto.title;
-        // const description = createBoardDto.description;
+    async createBoard(createBoardDto: CreateBoardDto): Promise <Board>{
         const {title, description} = createBoardDto;
 
-        const board: Board = {
-            id: uuid(),
+        const board = this.boardRepository.create({
             title,
             description,
             status: BoardStatus.PUBLIC
-        }
+        })
 
-        this.boards.push(board);
+        await this.boardRepository.save(board);
+
         return board;
     }
 
     // 특정게시글 불러오기
-    getBoardById(id: string): Board {
-        const found = this.boards.find((board) => board.id === id);
+    async getBoardById(id: number): Promise <Board> {
+        const found = await this.boardRepository.findOneBy({ id });
 
-        if(!found) {
-            throw new NotFoundException(`Can't find Board with id ${id}`);
+        if(!found){
+            throw new NotFoundException(`Can't find Board with id ${id}`)
         }
 
         return found;
     }
 
     // 특정게시글 지우기
-    deleteBoard(id: string): void{
-        const found = this.getBoardById(id);
-        this.boards = this.boards.filter((board) => board.id !== found.id);
-    }
+    
 
     // 특정게시글 수정
-    updateBoardStatus(id: string, status: BoardStatus): Board {
-        const board = this.getBoardById(id);
-        board.status = status;
-        return board;
-    }
+    
 
 }
