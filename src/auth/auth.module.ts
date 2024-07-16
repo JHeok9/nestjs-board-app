9443d615ff8d4e6a5dc1a8b7fa747513ecda2f52
JHeok <skdhd999@gmail.com>
2024-07-16 17:47:48 +0900

    JWT 설정

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,18 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserReopository } from './user.repository';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
     imports: [
+        PassportModule.register({defaultStrategy: 'jwt'}),
+        JwtModule.register({
+            secret: 'Secret1234', // 아무텍스트나 가능
+            signOptions:{
+                expiresIn: 60 * 60, // 한시간 유효
+            }
+        }),
         TypeOrmModule.forFeature([UserReopository])
     ],
     controllers: [AuthController],
