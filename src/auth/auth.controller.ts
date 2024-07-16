94444d305132c3f3bfaf9b7a88793b0493b0ce07
JHeok <skdhd999@gmail.com>
2024-07-16 15:43:46 +0900

    User 데이터 융효성 체크

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthCredentialsDto } from './dto/auth-credential.dto';
 
@@ -10,7 +10,7 @@ export class AuthController {
 
     // 유저 회원가입
     @Post('/signup')
-    signUp(@Body() authCredentialsDto: AuthCredentialsDto): Promise <void> {
+    signUp(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto): Promise <void> {
         return this.authService.signUp(authCredentialsDto);
     }
 
