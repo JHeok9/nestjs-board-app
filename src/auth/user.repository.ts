4eed28f3d8b6de0dcfddd37e6446f36e11cb4cec
JHeok <skdhd999@gmail.com>
2024-07-17 17:40:01 +0900

    데코레이터 추가

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -2,9 +2,10 @@ import { DataSource, Repository } from "typeorm";
 import { User } from "./user.entity";
 import { InjectDataSource } from "@nestjs/typeorm";
 import { AuthCredentialsDto } from "./dto/auth-credential.dto";
-import { ConflictException, InternalServerErrorException } from "@nestjs/common";
+import { ConflictException, Injectable, InternalServerErrorException } from "@nestjs/common";
 import * as bcrypt from 'bcryptjs';
 
+@Injectable()
 export class UserReopository extends Repository<User> {
     constructor(@InjectDataSource() dataSource: DataSource) {
         super(User, dataSource.createEntityManager());
